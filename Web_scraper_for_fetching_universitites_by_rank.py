8cee5d661acd58e0431a2e1089463b4e2048532b
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-13 14:43:51 +0530

    Final update

--- Web_scraper_for_fetching_universitites_by_rank.py
+++ Web_scraper_for_fetching_universitites_by_rank.py
@@ -22,31 +22,31 @@ browser= webdriver.Chrome('C:\Selenium\chromedriver', chrome_options=options)
 url=r'https://www.topuniversities.com/university-rankings/world-university-rankings/2019'
 browser.get(url)
     
-#TO GET THIS SPECIFIC XPATH WE HAVE TO SCROLL AS WELL AS HOVER TO ACTIVATE IT
+#TO GET THIS SPECIFIC XPATH WE HAVE TO SCROLL AS WELL AS hover_over TO ACTIVATE IT
 scroll = browser.find_element_by_xpath('//*[@id="qs-rankings_length"]/label/span[2]/span[2]')
-hover(scroll)
-browser.execute_script("window.scrollTo(0,2000)") 
+hover_over(scroll)
+scroll_fun(2000)
 
 #GET ALL THE RESULTS IN A SINGLE PAGE
 scr_all_menu=browser.find_element_by_xpath('//*[@id="qs-rankings_length"]/label/span[2]/span[2]')
-hover(scr_all_menu)
+hover_over(scr_all_menu)
 
 lag=browser.find_element_by_xpath('//*[@id="qs-rankings_length"]/label/span[2]/span[2]')
-hover(lag)
+hover_over(lag)
 
 scr_all_menu.click()
 
 #SELECTING ALL FROM DROP DOWN MENU
 scr_all=browser.find_element_by_xpath('//*[@id="qs-rankings_length"]/label/span[2]/div/div/span/span/ul/li[5]/span')
-hover(scr_all)
+hover_over(scr_all)
 scr_all.click()
 
 #SCROLLING BACK TO UP TO FETCH THE REGION WISE RESULT
-browser.execute_script("window.scrollTo(0,500)") 
+scroll_fun(500)
 
 #DROP DOWN MENU FOR SELECTING REGION 
 drop=browser.find_element_by_xpath('//*[@id="qs-rankings"]/thead/tr[3]/td[3]/div/div/span[2]/span[2]')
-hover(drop)
+hover_over(drop)
 drop.click() 
 
 #PRINTING LIST OF ITEMS IN THE DROP DOWN MENU
@@ -56,15 +56,6 @@ sel_print_region(region_list)
 #INPUT REGION
 region=input('Input the region: ')
 input_region(region)
-
-#Standard Function for search
-
-# def uni_search_by_name(uni_name):
-#     uni_search=browser.find_element_by_xpath('//*[@id="qs-rankings"]/thead/tr[3]/td[2]/div/input')
-#     uni_name=input('Enter_uni_name: ')
-#     uni_search.send_keys(uni_name)
-#     #Range of other functions doing their stuff here
-    
 
 #LIST OF ALL UNIVERSITIES
 uni= get_uni()
@@ -81,7 +72,6 @@ uni_data(ctry,rank,uni)
 #GET THE NAME OF THE UNIVERSITY SELECTED AS WELL AS CLICKING THE PAGE FOR MORE INFO
 #SELECT ANY INDEX FROM THE DATAFRAME
 index=input("Enter the index of the university you want to know more about: ")
-
 uni_info(index)
 
 #COMBINING LOCATION RELEVENT DETAILS OF THE UNIVERSITY TO FORM A DATAFRAME 
